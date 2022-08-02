d72e8d1ad7a9d46c9b14400ab1b196d2715d87e7
LemmonSterelogue <110470971+LemmonSterelogue@users.noreply.github.com>
2022-08-02 21:05:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,8 +25,8 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "惊喜": "./imgs/8e1bf7ab0755b53b55e383981ac98fd.jpg",
+        "还有惊喜": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
